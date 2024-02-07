1be4918089dcbfadd23ad90283efd4c6f8570ec1
Sakhawat Hossen <sakhawath2009@gmail.com>
2024-02-07 20:23:26 +0600

    ....

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,20 +4,19 @@ import LeftSidebar from "@/components/shared/sidebar/LeftSidebar";
 
 const Layout = ({children}: { children: React.ReactNode }) => {
   return (
-    <main className={"background-light850_dark100 relative grid grid-cols-12"}>
-      <div className={"col-span-12 h-[100px]"}>
-        <Navbar/>
+    <main className={"background-light850_dark100 relative"}>
+      <Navbar/>
+      <div className={"text-black dark:bg-dark-100  max-sm:hidden  font-bold"}>
+        <LeftSidebar/>
       </div>
-      <div className={"text-black dark:bg-dark-100  max-sm:hidden  font-bold col-start-1 col-span-1 sm:col-span-2 "}>
-        <LeftSidebar />
-      </div>
-      <section className={"col-start-3 col-span-7 max-sm:col-start-1 max-sm:col-span-12 flex min-h-screen  flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14"}>
+      <section
+        className={"flex min-h-screen  flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14"}>
         <div className={"max-auto w-full max-w-5xl"}>
           data
           {children}
         </div>
       </section>
-      <div className={"col-start-10 col-span-2"}>RightSideBard</div>
+      <div className={"text-black"}>RightSideBard</div>
 
 
       {/*toaster*/}
--- src/components/shared/sidebar/LeftSidebar.tsx
+++ src/components/shared/sidebar/LeftSidebar.tsx
@@ -8,14 +8,14 @@ import {usePathname} from "next/navigation";
 const LeftSidebar = () => {
   const pathmane = usePathname()
   return (
-    <section className={'flex flex-col gap-4 items-start pl-8 py-4 dark:text-gray-300 w-full'}>
+    <section className={'background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px] dark:text-gray-300 w-[105px] custom-scrollbar'}>
       {
         sidebarLinks.map(item => {
           const isActive = pathmane.includes(item.route) && item.route.length > 1 || pathmane === item.route
           console.log(isActive)
           return (
-            <div key={item.route} className={"w-full"}>
-              <Link href={item.route} className={`${isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark300_light900"} w-full flex  gap-4 bg-transparent p-4`}>
+            <div key={item.route} className={"flex flex-1 flex-col gap-6"}>
+              <Link href={item.route} className={`${isActive ? "primary-gradient rounded-lg text-light-900" : "text-dark300_light900"}  flex  gap-4 bg-transparent p-4`}>
                 <Image className={"invert-colors min-w-6"} src={item.imgURL} alt={item.label} height={24} width={24} />
                 <p className={`${isActive? "base-bold" : "base-medium"} max-lg:hidden text-nowrap`}>{item.label}</p>
               </Link>
